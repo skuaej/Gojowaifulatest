671afeff7f99f48d5ca008449fad6504a5f87f7d
skuaej <sumankumar821310@gmail.com>
2026-06-06 09:17:36 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -1,21 +1,34 @@
 from TEAMZYRO import *
 import importlib
-import logging
 from TEAMZYRO.modules import ALL_MODULES
 
+from flask import Flask
+from threading import Thread
+import os
+
+web = Flask(__name__)
+
+@web.route("/")
+def home():
+    return "Bot Running"
+
+def run_web():
+    web.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))
+    )
 
 def main() -> None:
+    Thread(target=run_web).start()
+
     for module_name in ALL_MODULES:
-        imported_module = importlib.import_module("TEAMZYRO.modules." + module_name)
+        importlib.import_module("TEAMZYRO.modules." + module_name)
+
     LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
 
     ZYRO.start()
-    application.run_polling(drop_pending_updates=True)
     send_start_message()
-    LOGGER("TEAMZYRO").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎MADE BY GOJOXNETWORK☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
-    )
+    application.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     main()
-    
